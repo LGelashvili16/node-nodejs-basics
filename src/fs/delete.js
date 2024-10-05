b4cb1072534d427f8a4fa7238733eece8ecfdc93
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-06 01:25:34 +0400

    feat: delete task is done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import * as fs from "node:fs";
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  function failFn() {
+    return console.error("FS operation failed");
+  }
+
+  if (!fs.existsSync("./files/fileToRemove.txt")) {
+    return failFn();
+  }
+
+  fs.unlink("./files/fileToRemove.txt", () => {});
 };
 
 await remove();
