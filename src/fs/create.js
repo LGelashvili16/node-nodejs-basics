9e7fbeb3c2720c4baac0cb125553bc7863c9f399
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-06 00:08:51 +0400

    init: first tast is done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import * as fs from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  // Write your code here
+  if (fs.existsSync("./files/fresh.txt")) {
+    return console.error("FS operation failed");
+  }
+
+  fs.writeFile("./files/fresh.txt", "I am fresh and young", (err) => {
+    if (err) {
+      console.error("Error occurred while creating the file!");
+      return;
+    }
+    console.log("The file has been created!");
+  });
 };
 
 await create();
