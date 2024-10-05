3204145d223bda34f73201f1631ba09f9c1aeb7b
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-06 01:01:29 +0400

    feat: copy task is done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import * as fs from "node:fs";
+
 const copy = async () => {
   // Write your code here
+  const filesDir = "./files";
+  const filesCopyDir = "./files_copy";
+
+  function failFn() {
+    return console.error("FS operation failed");
+  }
+
+  const isFilesDirPresent = fs.existsSync("./files");
+  const isFilesCopyDirPresent = fs.existsSync(filesCopyDir);
+
+  if (isFilesCopyDirPresent || !isFilesDirPresent) {
+    return failFn();
+  }
+
+  fs.mkdirSync(filesCopyDir);
+
+  fs.readdir(filesDir, (err, files) => {
+    for (let file of files) {
+      fs.copyFile(`${filesDir}/${file}`, `${filesCopyDir}/${file}`, () => {});
+    }
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -2,14 +2,18 @@ import * as fs from "node:fs";
 
 const create = async () => {
   // Write your code here
-  if (fs.existsSync("./files/fresh.txt")) {
+
+  function failFn() {
     return console.error("FS operation failed");
+  }
+
+  if (fs.existsSync("./files/fresh.txt")) {
+    return failFn();
   }
 
   fs.writeFile("./files/fresh.txt", "I am fresh and young", (err) => {
     if (err) {
-      console.error("Error occurred while creating the file!");
-      return;
+      return failFn();
     }
     console.log("The file has been created!");
   });
