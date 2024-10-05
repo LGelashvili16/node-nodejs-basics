031adb7c01073a99c8a935e78411745659c94b8f
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-06 01:17:13 +0400

    feat: rename task is done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import * as fs from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  function failFn() {
+    return console.error("FS operation failed");
+  }
+
+  const isWrongFileNamePresent = fs.existsSync("./files/wrongFilename.txt");
+  const isProperFileNamePresent = fs.existsSync("./files/properFilename.md");
+
+  if (!isWrongFileNamePresent || isProperFileNamePresent) {
+    return failFn();
+  }
+
+  fs.rename("./files/wrongFilename.txt", "./files/properFilename.md", () => {});
 };
 
 await rename();
