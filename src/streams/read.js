7f1ae60d579357515ff3d1b42b7e6113e25a0505
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-07 00:31:05 +0400

    feat: streams task is done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const stream = fs.createReadStream(
+    path.join(__dirname, "files", "fileToRead.txt")
+  );
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log(" Finished");
+  });
 };
 
 await read();
