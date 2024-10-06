48e848d9a49363d185e184e7f78a0182bfd36cee
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-06 13:27:39 +0400

    feat: env.js task is done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  process.env.RSS_name1 = "value1";
+  process.env.RSS_name2 = "value2";
+
+  Object.entries(process.env).filter((key) => {
+    if (key[0].startsWith("RSS")) {
+      console.log(`${key[0]}=${key[1]}`);
+    }
+  });
 };
 
 parseEnv();
