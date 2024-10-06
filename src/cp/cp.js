71e3a840d902acf6f9fc83eba6c32167b301141f
LGelashvili <100406685+LGelashvili16@users.noreply.github.com>
2024-10-07 02:30:16 +0400

    feat: wt task is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,8 @@
+import { spawn } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);
